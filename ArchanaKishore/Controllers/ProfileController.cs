dc76fac167faa976676155d3d90b598da6063f49
unknown <archanakishore0611@outlook.com>
2018-06-03 01:53:07 -0400

    my first ever commit. Hurray !!!

---
+++ ArchanaKishore/Controllers/ProfileController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ArchanaKishore.Models;
+
+namespace ArchanaKishore.Controllers
+{
+    public class ProfileController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult Projects()
+        {
+            return View();
+        }
+
+        public IActionResult Interests()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ ArchanaKishore/wwwroot/js/app.js
@@ -0,0 +1,54 @@
+﻿$(document).ready(function () {
+    $('#resumeIcon').addClass('active-item');
+    resumeIconOnCLick();
+    projectsIconOnCLick();
+    interestsIconOnClick();
+    autoPlayYouTubeModal();
+});
+
+function resumeIconOnCLick() {
+    $('#resumeIcon').on("click", function () {
+        $('#resumeIcon').addClass('active-item');
+        $('#projectsIcon').removeClass('active-item');
+        $('#interestsIcon').removeClass('active-item');
+        $('#ResumePreview').show();
+        $('#ProjectsPreview').hide();
+        $('#InterestsPreview').hide();
+    });
+}
+
+function projectsIconOnCLick() {
+    $('#projectsIcon').on("click", function () {
+        $('#projectsIcon').addClass('active-item');
+        $('#resumeIcon').removeClass('active-item');
+        $('#interestsIcon').removeClass('active-item');
+        $('#ResumePreview').hide();
+        $('#ProjectsPreview').show();
+        $('#InterestsPreview').hide();
+    });
+}
+
+function interestsIconOnClick() {
+    $('#interestsIcon').on("click", function () {
+        $('#projectsIcon').removeClass('active-item');
+        $('#resumeIcon').removeClass('active-item');
+        $('#interestsIcon').addClass('active-item');
+        $('#ResumePreview').hide();
+        $('#ProjectsPreview').hide();
+        $('#InterestsPreview').show();
+    });
+}
+
+function autoPlayYouTubeModal() {
+    //alert('Entered');
+    var trigger = $("#videoLink").find('[data-toggle="modal"]');
+    trigger.click(function () {
+        var theModal = $(this).data("target"),
+            videoSRC = $(this).attr("data-theVideo"),
+            videoSRCauto = videoSRC + "?autoplay=1";
+        $(theModal + ' iframe').attr('src', videoSRCauto);
+        $(theModal + ' button.close').click(function () {
+            $(theModal + ' iframe').attr('src', videoSRC);
+        });
+    });
+}
